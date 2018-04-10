1b4ad8e640da5b1db5d1d4084d4e4b8524a7275f
Jakub Czapiga <jakub.czapiga@outlook.com>
2018-04-10 22:29:38 +0200

    Probably working

--- Ex4/Part2/main.c
+++ Ex4/Part2/main.c
@@ -4,55 +4,76 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <errno.h>
 
-static int pids[10] = {0};
-static int currentIdx = 0;
-static int processesRequests[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static int N = 0;
+static int M = 0;
+static int M_acc = 0;
+static pid_t* pidsArr = NULL;
+static int* pidsRequests = NULL;
+static int dead = 0;
 
 int canRun = 0;
 
-int random(int min_number, int max_number) {
+int randomInt(int min_number, int max_number) {
     return (rand() % (max_number + 1 - min_number) + min_number);
 }
 
 void slaveHandler(int signum) {
-    if(signum == SIGUSR1 || canRun == 0) {
+    if (signum == SIGUSR1 && canRun == 0) {
+        fprintf(stderr, "Can Go\n");
         canRun = 1;
     }
 }
 
-void slaveRunner() {
+void slaveRunner(void) {
     srand(time(NULL));
 
-    int secs = rand()%10+1;
+    signal(SIGUSR1, slaveHandler);
+
+    int secs = rand() % 10 + 1;
+
+    fprintf(stderr, "FromSlave: Secs = %d\n", secs);
 
     sleep(secs);
-    
-    sigqueue(getppid(), SIGUSR1, (const union sigval)NULL);
 
-    sigset_t sa;
-    sigfillset(&sa);
-    sigdelset(&sa, SIGINT);
-    sigdelset(&sa, SIGUSR1);
-    sigsuspend(&sa);
+    // union sigval sgv;
+    // sgv.sival_ptr = NULL;
 
-    while(canRun == 0) {}
+    fprintf(stderr, "Sending to: %d\n", getppid());
 
-    sigqueue(getppid(), random(SIGRTMIN, SIGRTMAX), (const union sigval)NULL);
+    kill(getppid(), SIGUSR1);
+
+    while (canRun == 0) {
+        usleep(50);
+    }
+
+    fprintf(stderr, "Hija\n");
+
+    fprintf(stderr, "PID: %d\n", getppid());
+    kill(getppid(), randomInt(SIGRTMIN, SIGRTMAX));
+
+    fprintf(stderr, "Hija2\n");
+
+    fprintf(stderr, "Exiting %d\n", secs);
 
     exit(secs);
 }
 
-void masterHandler(int signum, siginfo_t* info, void* _) {
+void masterHandler(int signum, siginfo_t* info, void* data) {
+    (void)data;
     int pid = info->si_pid;
+    union sigval sgv;
+    sgv.sival_ptr = NULL;
 
     printf("PROCESS: %d SIGNAL %d\n", pid, signum);
 
     int idx = -1;
     for (int i = 0; i < 10; ++i) {
-        if (pids[i] == pid) {
+        if (pidsArr[i] == pid) {
             idx = i;
             break;
         }
@@ -60,13 +81,78 @@ void masterHandler(int signum, siginfo_t* info, void* _) {
 
     if (idx == -1) return;
 
-    if(processesRequests[idx] < 10) {
-        processesRequests[idx]++;
+    if (M_acc >= M) {
+        sigqueue(pid, SIGUSR1, sgv);
+    }
 
-        if(processesRequests[idx] == 10) {
-            sigqueue(pid, SIGUSR1, (const union sigval)NULL);
+    pidsRequests[idx]++;
+    M_acc++;
+
+    if (M_acc >= M) {
+        for (int i = 0; i < N; ++i) {
+            if (pidsRequests[i] != 0) {
+                sigqueue(pidsArr[i], SIGUSR1, sgv);
+            }
         }
     }
+}
+
+void master_sigrt_handler(int signum, siginfo_t* info, void* data) {
+    (void)data;
+    fprintf(stderr, "PROCESS: %d, SIGNAL: RT %d\n", info->si_pid, signum);
+}
+
+void master_sigchld_handler(int signum) {
+    (void)signum;
+    int status = 0;
+    pid_t chldpid = 0;
+
+    fprintf(stderr, "SIGCHLD!\n");;
+
+    while (1) {
+        chldpid = waitpid(-1, &status, WNOHANG);
+
+        if (chldpid <= 0) {
+            return;
+        }
+
+        printf("PROCESS: %d, STATUS: %d, RETURN: %d\n", chldpid, status,
+               WEXITSTATUS(status));
+
+        dead++;
+    }
+}
+
+void masterRunner(void) {
+    signal(SIGCHLD, master_sigchld_handler);
+
+    struct sigaction sa;
+    sa.sa_sigaction = master_sigrt_handler;
+    sigemptyset(&sa.sa_mask);
+
+    for (int i = SIGRTMIN; i <= SIGRTMAX; ++i) {
+        sigaction(i, &sa, NULL);
+    }
+
+    sa.sa_sigaction = masterHandler;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    fprintf(stderr, "Starting master %d\n", getpid());
+
+    // while (dead != N) {
+    //     usleep(50);
+    // }
+
+    int pppid;
+    while ((pppid = waitpid(-1, NULL, 0)) > 0) {
+        fprintf(stderr, "Died: %d\n", pppid);
+    }
+
+    fprintf(stderr, "Is that all?\n");
+}
+
+void ate(void) {
+    fprintf(stderr, "DUPA\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -75,27 +161,39 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    int N = itoa(argv[1]);
-    int M = itoa(argv[2]);
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
 
     if (N <= 0 || M <= 0) {
         fprintf(stderr, "Invalid arguments.\n");
         return -1;
     }
 
-    for (currentIdx = 0; currentIdx < 10; ++currentIdx) {
-        pids[currentIdx] = fork();
+    pidsArr = (pid_t*)calloc(N, sizeof(pid_t));
+    pidsRequests = (pid_t*)calloc(N, sizeof(int));
 
-        if (pids[currentIdx] == 0) {
+    fprintf(stderr, "N = %d\nM = %d\n", N, M);
+
+    for (int i = 0; i < N; ++i) {
+        pidsArr[i] = fork();
+
+        if (pidsArr[i] == 0) {
             slaveRunner();
             exit(0);
-        } else if (pids[currentIdx] < 0) {
+        } else if (pidsArr[i] < 0) {
             fprintf(stderr, "Failed to fork process.\n");
             exit(1);
         }
+
+        usleep(50);
     }
 
     masterRunner();
+
+    free(pidsArr);
+    free(pidsRequests);
+
+    fprintf(stderr, "Ending\n");
 
     return 0;
 }
